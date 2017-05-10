b8d813eedc6cb763ab831583473891a425aa5e68
Ignacio Escobar <inec_14@hotmail.com>
2017-05-10 07:16:22 -0300

    Barebones automata

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Nodo de resolucion
-struct nodo{
-    float valor;
-    struct nodo* padre; 
-    struct nodo* hijo;
-};
 //Variables de automata
 int caracter, operacion = '+';
 int centinelaNegatividad = 0;
@@ -14,13 +8,11 @@ float valorAcumulado = 0;
 void automata          ();
 void inicial           ();
 void negativo          ();
-void construirDigito   (int caracter, float* valorAcumulado);
-void operar            (int caracter, float *valorAcumulado,int *centinelaNegatividad );
+void construirDigito   ();
+void operar            ();
 void rechazo           ();
 void aceptacion        ();
-void crearNodo         (int operacion,float valor);
-struct nodo* nuevoNodo (float x);
-float resolver          ();
+
 
 int main(){
     automata();
@@ -43,9 +35,7 @@ void inicial(){
 }
 
 //Estado 1
-void negativo(int *centinelaNegatividad){
-    if (*centinelaNegatividad == 1){*centinelaNegatividad = 0;}
-    if (*centinelaNegatividad == 0){*centinelaNegatividad = 1;}
+void negativo(){
     //Trancisiones
     caracter = getchar();
     if((caracter>='1') && (caracter<='9')) {construirDigito(caracter,&valorAcumulado);return;}
@@ -54,8 +44,7 @@ void negativo(int *centinelaNegatividad){
 };
 
 //Estado 2 - Estado de Aceptacion
-void construirDigito(int caracter, float *valorAcumulado){
-    *valorAcumulado = (caracter-'0') + *valorAcumulado*10;
+void construirDigito(){
     //Trancisiones
     caracter = getchar();
     if((caracter>='0') && (caracter<='9')) {construirDigito(caracter,valorAcumulado);return;}
@@ -67,13 +56,7 @@ void construirDigito(int caracter, float *valorAcumulado){
 }
 
 //Estado 0' - Estado de reingreso
-void operar(int caracter, float *valorAcumulado,int *centinelaNegatividad ){
-    crearNodo(operacion,((*valorAcumulado)*(*centinelaNegatividad)));
-    operacion = caracter;
-    *centinelaNegatividad = 0;
-    if (caracter == '-'){*centinelaNegatividad = 1;operacion = '+';}
-    printf("v: %f\n", *valorAcumulado);
-    *valorAcumulado = 0;
+void operar(){
     //Trancisiones
     caracter = getchar();
     if(caracter=='-')                     {negativo(centinelaNegatividad);return;};
@@ -90,81 +73,3 @@ void aceptacion(){
     printf("Resultado: %f \n",r);
 }
 
-//Resolucion
-struct nodo* frenteDeSuma           = NULL;
-struct nodo* fondoDeSuma            = NULL;
-struct nodo* frenteDeMultiplicacion = NULL;
-struct nodo* fondoDeMultiplicacion  = NULL;
-struct nodo* frenteDeDivision       = NULL;
-struct nodo* fondoDeDivision        = NULL;
-struct nodo* punteroDeOperacion     = NULL;
-struct nodo* nodoTemp               = NULL;
-
-void crearNodo(int operacion,float valor){
-    nodoTemp = nuevoNodo(valor);
-    if (operacion == '+'){
-        if (frenteDeSuma == NULL){
-            frenteDeSuma = nodoTemp;
-        }else{
-            fondoDeSuma -> hijo = nodoTemp;
-            fondoDeSuma = nodoTemp;
-        }
-    };
-    if (operacion == '*'){
-        if (frenteDeMultiplicacion == NULL){
-            frenteDeMultiplicacion = nodoTemp;
-        }else{
-            fondoDeMultiplicacion -> hijo = nodoTemp;
-            fondoDeMultiplicacion = nodoTemp;
-        }
-    };
-    if (operacion == '/'){
-        if (frenteDeDivision == NULL){
-            frenteDeDivision = nodoTemp;
-        }else{
-            fondoDeDivision -> hijo = nodoTemp;
-            fondoDeDivision = nodoTemp;
-        }
-    };
-    nodoTemp -> padre = punteroDeOperacion;
-    if (punteroDeOperacion == NULL){
-        punteroDeOperacion = nodoTemp;
-    }    
-};
-
-struct nodo* nuevoNodo(float x){
-    struct nodo* nuevoNodo = (struct nodo*)malloc(sizeof(struct nodo));
-    nuevoNodo -> valor = x;
-    nuevoNodo -> padre = NULL; 
-    nuevoNodo -> hijo  = NULL;
-    return nuevoNodo;
-}
- 
-float resolver(){
-    struct nodo* iterador = frenteDeMultiplicacion;
-    struct nodo* temporal;
-    float  resultado = 0;
-    while(iterador != NULL){
-        iterador -> padre -> valor = (iterador -> padre -> valor)*(iterador -> valor); 
-        iterador -> hijo -> padre  =  iterador -> padre;
-        temporal = iterador;
-        iterador = iterador -> hijo;
-        free(temporal);
-    }
-    iterador = frenteDeDivision;
-    while(iterador != NULL){
-        iterador -> padre -> valor = (iterador -> padre -> valor)/(iterador -> valor); 
-        iterador -> hijo -> padre  =  iterador -> padre;
-        temporal = iterador;
-        iterador = iterador -> hijo;
-        free(temporal);
-    }
-    iterador = frenteDeSuma;
-    while(iterador != NULL){
-        resultado += iterador -> valor;
-        temporal = iterador;
-        iterador = iterador -> hijo;
-        free(temporal);
-    }
-    return resultado;
-}
